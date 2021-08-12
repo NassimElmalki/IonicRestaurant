713af5ea4a73fcf1c3f7018dd97aadf64fd418a0
yaskillin <45403305+yaskillin@users.noreply.github.com>
2021-08-12 11:02:52 +0100

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'profil',
     loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+
 ];
 
 @NgModule({
